11dbb00ab99b074ff1efa6b06c1d6058c3cad29c
s.sheikh <mssheikh1377@gmail.com>
2021-07-26 19:22:48 +0430

    Repository interface and Class Done

---
+++ Object13.DataLayer/Repository/GenericRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Object13.DataLayer.Context;
+using Object13.DataLayer.Models.Main;
+
+namespace Object13.DataLayer.Repository
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity :BaseModel
+    {
+        #region Ctor
+        private Object13Context _context;
+        private DbSet<TEntity> _dbSet;
+        public GenericRepository(Object13Context context)
+        {
+            _context = context;
+            _dbSet = this._context.Set<TEntity>();
+        }
+        #endregion
+
+        public IQueryable<TEntity> GetEntitiesQuery()
+        {
+            return _dbSet.AsQueryable();
+        }
+
+        public async Task<TEntity> GetEntityById(long entityId)
+        {
+            return await _dbSet.SingleOrDefaultAsync(e => e.Id == entityId);
+        }
+
+        public async Task AddEntity(TEntity entity)
+        {
+            entity.CreateDate = DateTime.Now;
+            entity.LastUpdateDate = entity.CreateDate;
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void UpdateEntity(TEntity entity)
+        {
+            entity.LastUpdateDate = DateTime.Now;
+            _dbSet.Update(entity);
+        }
+
+        public void RemoveEntity(TEntity entity)
+        {
+            entity.IsDelete = true;
+            UpdateEntity(entity);
+        }
+
+        public async Task RemoveEntity(long entityId)
+        {
+            var entity = await GetEntityById(entityId);
+            RemoveEntity(entity);
+        }
+
+        public async Task SaveChanges()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+    }
+}
---
+++ Object13.DataLayer/Repository/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Object13.DataLayer.Models.Main;
+
+namespace Object13.DataLayer.Repository
+{
+    public interface IGenericRepository<TEntity> : IDisposable where TEntity:BaseModel
+    {
+        IQueryable<TEntity> GetEntitiesQuery();
+        Task<TEntity> GetEntityById(long entityId);
+        Task AddEntity(TEntity entity);
+        void UpdateEntity(TEntity entity);
+        void RemoveEntity(TEntity entity);
+        Task RemoveEntity(long entityId);
+        Task SaveChanges();
+    }
+}
